058b033a4aaf17938343bd6a2f8383a25e958ff1
deepy <kadanapool@gmail.com>
2024-06-18 16:12:28 +0200

    adding dist v5

--- assets/formDataStore.7bb561a8.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e}from"./index.ee0b6fbb.js";const r=e("formData",{state:()=>({address:""}),getters:{getAddress(){return this.address}},actions:{updateAddress(s){this.address=s}}});export{r as u};
