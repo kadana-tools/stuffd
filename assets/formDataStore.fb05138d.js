aa799f26d1ea230cfa47d0962c92a1530128bd3e
deepy <kadanapool@gmail.com>
2024-06-22 09:06:59 +0200

    adding dist v13 update of MiningSetup.vue by removing wrong script section

---
+++ assets/formDataStore.fb05138d.js
@@ -0,0 +1,1 @@
+import{a2 as e}from"./index.77dab4f4.js";const r=e("formData",{state:()=>({address:""}),getters:{getAddress(){return this.address}},actions:{updateAddress(s){this.address=s}}});export{r as u};
