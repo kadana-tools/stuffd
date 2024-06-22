3522fdc1a3632ce0ce05219fe99dd5280ad5cdf2
deepy <kadanapool@gmail.com>
2024-06-22 08:31:13 +0200

    adding dist v12 update of ProjectTotals.vue to have proper API link

--- assets/formDataStore.354ff662.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e}from"./index.80a6f9ed.js";const r=e("formData",{state:()=>({address:""}),getters:{getAddress(){return this.address}},actions:{updateAddress(s){this.address=s}}});export{r as u};
---
+++ assets/formDataStore.ee604827.js
@@ -0,0 +1,1 @@
+import{a2 as e}from"./index.5c53596a.js";const r=e("formData",{state:()=>({address:""}),getters:{getAddress(){return this.address}},actions:{updateAddress(s){this.address=s}}});export{r as u};
