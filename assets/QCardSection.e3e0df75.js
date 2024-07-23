70f9c41785ca59aa9882bb6e2db08b01dc02c120
deepy <kadanapool@gmail.com>
2024-07-23 17:10:07 +0200

    adding dist with error handling

---
+++ assets/QCardSection.e3e0df75.js
@@ -0,0 +1,1 @@
+import{c as r,d as e,h as n}from"./index.193de406.js";import{h as s}from"./render.d0230eea.js";var d=r({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:o}){const a=e(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:a.value},s(o.default))}});export{d as Q};
---
+++ assets/use-dark.3853ec7e.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index.193de406.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
